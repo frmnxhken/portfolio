ae918f6cc9bb21775cfc6d0434ec1270f9371a03
frmnxhken <frmnxhken@gmail.com>
2025-03-26 12:32:14 +0700

    Update font size on quote

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -29,7 +29,7 @@ const Quote = () => {
         className='max-w-[1200px] mx-auto px-4 pt-24 min-h-screen flex flex-col justify-center sticky top-0'
       >
         <div className='w-[90%] mx-auto sm:w-[80%] text-left sm:text-center'>
-          <h1 className='text-5xl sm:text-7xl uppercase font-bold supports-[animation-timeline]:reveal-text-colorfull'>
+          <h1 className='text-4xl sm:text-7xl uppercase font-bold supports-[animation-timeline]:reveal-text-colorfull'>
             Being alone is not synonymous with being lonely
           </h1>
         </div>
